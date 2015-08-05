b00e6abc1751d6da5e6d8ccdd6d00978038fa9dd
zhaochunyang0726 <zhaochunyang0726@163.com>
2015-08-05 14:29:22 +0800

    Initial Commit

---
+++ ZCYMultipleChoice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZCYMultipleChoice
+//
+//  Created by zhaochunyang on 15/8/5.
+//  Copyright (c) 2015年 zhaochunyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZCYMultipleChoice/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ZCYMultipleChoice
+//
+//  Created by zhaochunyang on 15/8/5.
+//  Copyright (c) 2015年 zhaochunyang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
